8a36f204a2b058637e2fcba7e80e9996f5337431
GreenLuimonade <sanjunior08@gmail.com>
2024-10-24 11:05:27 -0500

    log in

--- app/src/main/java/com/personeriatocancipa/app/Bienvenida.kt
+++ app/src/main/java/com/personeriatocancipa/app/Bienvenida.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Bienvenida : AppCompatActivity() {
 
@@ -62,16 +63,59 @@ class Bienvenida : AppCompatActivity() {
                         val intent = Intent(this@Bienvenida, Cliente::class.java)
                         finish()
                         startActivity(intent)
-                    }else{
-                        Toast.makeText(
-                            this@Bienvenida,
-                            "¡Hubo un error!",
-                            Toast.LENGTH_SHORT
-                        ) .show()
-                    }
+                    }else {
+                        mAuth.signInWithEmailAndPassword(correo, clave)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    val currentUser = mAuth.currentUser
+                                    if (currentUser != null) {
+                                        val db = FirebaseFirestore.getInstance()
+                                        db.collection("usuarios").document(currentUser.uid).get()
+                                            .addOnSuccessListener { document ->
+                                                if (document != null) {
+                                                    val role = document.getString("rol")
+                                                    // porque en kotlin el switch case es when?
+                                                    when (role) {
+                                                        "0" -> {
+                                                            val intent = Intent(this@Bienvenida, Cliente::class.java)
+                                                            startActivity(intent)
+                                                        }
+                                                        "1" -> {
+                                                            val intent = Intent(this@Bienvenida, Abogado::class.java)
+                                                            startActivity(intent)
+                                                        }
+                                                        "2" -> {
+                                                            val intent = Intent(this@Bienvenida, Admin::class.java)
+                                                            startActivity(intent)
+                                                        }
+                                                        else -> {
+                                                            Toast.makeText(
+                                                                this@Bienvenida,
+                                                                "no se como pero el rol no existe",
+                                                                Toast.LENGTH_SHORT
+                                                            ).show()
+                                                        }
+                                                    }
+                                                    finish()
+                                                } else {
+                                                    Toast.makeText(
+                                                        this@Bienvenida,
+                                                        "¡Hubo un error al obtener el rol!",
+                                                        Toast.LENGTH_SHORT
+                                                    ).show()
+                                                }
+                                            }
+                                    }
+                                } else {
+                                    // Si el login falla, mostrar un mensaje
+                                    Toast.makeText(
+                                        this@Bienvenida,
+                                        "¡Hubo un error!",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
                 }
         }
     }
 
 
-}
+}}}}
