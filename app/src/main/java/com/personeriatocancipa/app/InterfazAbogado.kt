8b63f3b3cb2a6fb2e5b3f2df9bb639d8fe357cc6
Rickay1121 <116464125+ricardocortesc@users.noreply.github.com>
2024-11-18 22:36:57 -0500

    Consulta de Citas para Cliente y Abogado

--- app/src/main/java/com/personeriatocancipa/app/InterfazAbogado.kt
+++ app/src/main/java/com/personeriatocancipa/app/InterfazAbogado.kt
@@ -1,7 +1,9 @@
 package com.personeriatocancipa.app
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -13,14 +15,23 @@ import com.google.firebase.database.ValueEventListener
 class InterfazAbogado : AppCompatActivity() {
     private lateinit var txtUsuario: TextView
     private lateinit var mAuth: FirebaseAuth
+    private lateinit var btnConsultarCitas: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abogado)
 
         mAuth = FirebaseAuth.getInstance()
+        btnConsultarCitas = findViewById(R.id.btnConsultarCitas)
 
         cargarNombre()
+
+        btnConsultarCitas.setOnClickListener{
+            // Redirigir a la actividad de consulta de citas
+            val intent = Intent(this@InterfazAbogado, ConsultarCitasAbogado::class.java)
+            finish()
+            startActivity(intent)
+        }
     }
 
     private fun cargarNombre() {
