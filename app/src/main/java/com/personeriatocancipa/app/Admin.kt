5e3607b9973d7e0034b271725d7eb61bf99ff8f6
GreenLuimonade <sanjunior08@gmail.com>
2024-11-18 21:58:01 -0500

    Admin

---
+++ app/src/main/java/com/personeriatocancipa/app/Admin.kt
@@ -0,0 +1,24 @@
+package com.personeriatocancipa.app
+
+class Admin {
+    var cedula: String? = null // LLave primaria
+    var nombreCompleto: String? = null // LLave primaria
+    var correo: String? = null
+    var estado: String? = null // Activo, inactivo
+
+    constructor()
+
+    constructor(
+        cedula: String?,
+        nombreCompleto: String?,
+        correo: String?,
+        estado: String?,
+        uid: String?
+    ) {
+        this.cedula = cedula
+        this.nombreCompleto = nombreCompleto
+        this.correo = correo
+        this.estado = estado
+    }
+
+}
