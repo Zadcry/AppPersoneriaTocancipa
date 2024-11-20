dbb9939232d87d56e9125c4883c1d4a41a7889b1
carlosazmora <carloszulmor@unisabana.edu.co>
2024-11-20 12:08:21 -0500

    Añade botones de salida y correción estética

--- app/src/main/java/com/personeriatocancipa/app/ConsultarCitasAbogado.kt
+++ app/src/main/java/com/personeriatocancipa/app/ConsultarCitasAbogado.kt
@@ -2,6 +2,7 @@ package com.personeriatocancipa.app
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -16,12 +17,14 @@ class ConsultarCitasAbogado : AppCompatActivity() {
     private lateinit var adapter: CitaAdapterAbogado
     private lateinit var databaseReference: DatabaseReference
     private lateinit var auth: FirebaseAuth
+    private lateinit var btnSalir: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_consultar_citas_abogado)
 
         recyclerView = findViewById(R.id.recyclerViewCitaAbogado)
+        btnSalir = findViewById(R.id.btnSalir)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         citasList = mutableListOf()
@@ -32,6 +35,10 @@ class ConsultarCitasAbogado : AppCompatActivity() {
         databaseReference = FirebaseDatabase.getInstance().getReference("citas")
 
         cargarCitas()
+
+        btnSalir.setOnClickListener {
+            finish()
+        }
     }
 
     private fun cargarCitas() {
